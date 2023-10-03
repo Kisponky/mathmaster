c7fbaaaefc7deb43cca5ebc75ab14d90a94b01ba
LioMes1001 <tamasnadas04@gmail.com>
2023-10-03 19:43:53 +0200

    Használható hamburger menü

--- Frontend/index.php
+++ Frontend/index.php
@@ -4,38 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
-        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
-        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
-        crossorigin="anonymous"></script>
-    <!-- Bootstrap CSS beillesztése (online CDN) -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <!-- Stíluslap -->
-    
-    <link rel="stylesheet" href="styles.css"> <!-- Ebben a fájlban van a saját CSS kódod -->
-    <link rel="shortcut icon" href="/Frontend/kepek/owl-47526.svg">
-    <!-- Oldal faviconjának beállítása -->
-    <title>MathMaster - Matematikai gyakorló feladatok</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <title>Document</title>
 </head>
 
 <body>
     <header>
         <nav class="navbar navbar-expand-sm navbar-light bg-light">
             <a href="#" class="navbar-brand mb-0 h1">
-                <img class="MathMaster_Logo d-inline-block aling-top" src="/Frontend/kepek/owl-47526.svg" alt="MathMaster Logó">
-                MathMaster
+                <img class="MathMaster_Logo d-inline-block align-top" src="img/owl-47526.svg" alt="MathMaster Logó">
             </a>
             <button class="navbar-toggler second-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation">
                 <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent23">
-                <ul class="navbar-nav ml-auto">
+                <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Kezdőlap</a>
                     </li>
@@ -52,17 +41,19 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-12 logo_nev">
-                <img src="/Frontend/kepek/owl-47526.svg" alt="MathMaster Logó">
-                <p class="nev">MathMaster</p>
+            <div class="col-lg-12 logo_nev">
+                <img src="/Frontend/img/owl-47526.svg" alt="MathMaster Logó">
+                <h1 class="nev">MathMaster</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 educator">
+                <img src="/Frontend/img/" alt="MathMaster Logó">
             </div>
         </div>
     </div>
 
-    <!-- Bootstrap JavaScript és jQuery beillesztése (online CDN) -->
-    
-    <!-- A saját JavaScript kód hivatkozása (ez az, amivel aktiválod az ikon animációkat) -->
-    <script src="index.js"></script> <!-- Itt tárolhatod el a saját JavaScript kódodat -->
+    <script src="js/index.js"></script>
 </body>
 
 </html>
