3fc72f9b29aa97ea22ab0c747a4cc5734e59ea59
LioMes1001 <tamasnadas04@gmail.com>
2023-10-04 22:44:08 +0200

    index.php (beta)

--- Frontend/index.php
+++ Frontend/index.php
@@ -5,13 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
-
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
-    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="shortcut icon" href="img/owl-47526.svg">
+
+    <title>MathMaster</title>
 </head>
 
 <body>
@@ -19,6 +21,7 @@
         <nav class="navbar navbar-expand-sm navbar-light bg-light">
             <a href="#" class="navbar-brand mb-0 h1">
                 <img class="MathMaster_Logo d-inline-block align-top" src="img/owl-47526.svg" alt="MathMaster Logó">
+                MathMaster 
             </a>
             <button class="navbar-toggler second-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation">
                 <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
@@ -40,18 +43,71 @@
     </header>
 
     <div class="container">
-        <div class="row">
-            <div class="col-lg-12 logo_nev">
+        <div class="row logo_row">
+            <div class="col-lg-12 text-center logo_nev">
                 <img src="/Frontend/img/owl-47526.svg" alt="MathMaster Logó">
                 <h1 class="nev">MathMaster</h1>
             </div>
         </div>
+        <div class="row tanulas_row">
+            <div class="col-lg-12 text-center">
+                <h3 class="tanulas">Kezdjünk neki a tanulásnak kis baglyocska!</h3>
+                <img class="mb-5 mt-3 educator" src="img/undraw_educator_re_ju471.svg" alt=""><br>
+            </div>
+        </div>
         <div class="row">
-            <div class="col-lg-12 educator">
-                <img src="/Frontend/img/" alt="MathMaster Logó">
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-3 col-md-12 text-center">
+                <a href="html/1-2.html"><button class="btn btn-danger">1-2. osztály</button></a>
+            </div>
+            <div class="col-lg-3 col-md-12 text-center">
+                <a href="html/3-4.html"><button class="btn btn-danger">3-4. osztály</button></a>
+            </div>
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-3">
+            </div>
+            <div class="col-lg-3 col-md-12 text-center">
+                <button class="btn btn-secondary" title="Jelenleg még nem elérhető">5-6. osztály</button>
+            </div>
+            <div class="col-lg-3 col-md-12 text-center">
+                <button class="btn btn-secondary" title="Jelenleg még nem elérhető">7-8. osztály</button>
+            </div>
+            <div class="col-lg-3">
+            </div>
+        </div>
+        <div class="row">
+            
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="rolunk_felirat">Rólunk:</h2>
+
+                <p class="rolunk">Üdvözöljük a MathMaster online platformján! Válassz minket, mert szenvedélyünk az oktatás, és ezt a rendkívül hasznos tanulási oldalt életre hoztuk azért, hogy segítsünk neked a matematika világában.</p>
+
+                <p class="rolunk">Tudjuk, hogy a matematika néha kihívást jelent, de mi itt vagyunk, hogy segítsünk neked.</p>
+
+                <button class="btn btn-secondary rolunk_btn" title="Jelenleg még nem elérhető">Tudj meg többet</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="kapcs">Kapcsolatok:</h2>
+
+                <p class="rolunk">Üdvözöljük a MathMaster gyakorló platformunkon! Itt vagyunk, hogy segítsünk, és várjuk észrevételeit, kérdéseit vagy javaslatait a szolgáltatásunkkal kapcsolatban.</p>
+
+                <p class="rolunk">Hogyan érhet el minket?
+                    Bármikor szívesen fogadjuk visszajelzéseit és észrevételeit. </p>
+
+                <button class="btn btn-secondary rolunk_btn" title="Jelenleg még nem elérhető">Kapcsolat felvétel</button>
             </div>
         </div>
     </div>
+
+    <footer>
+        &copy 2023 MathMaster
+    </footer>
 
     <script src="js/index.js"></script>
 </body>
