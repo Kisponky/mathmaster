03cc7f3d58418dc3552d314d49c36869daab958c
Kisponky <ponkhazibanri@gmail.com>
2023-10-08 15:50:01 +0200

    Admin felület felhasználó autentikációval. Új admin felvétel lehetőség hozzáadva az admin.php-hez.

--- backend/index.php
+++ backend/index.php
@@ -5,7 +5,9 @@ session_start(); // sessions munkamenet megnyitása
 if (isset($_SESSION['user_fnev'])) {
     $fnev = $_SESSION['user_fnev']; // Változtattam $fnev-t $_SESSION['user_email']-ra, mivel a bejelentkezett felhasználónak van e-mail címe
     echo '<a href="logout.php">KIJELNTKEZÉS</a>'; // Kijelentkezés link megjelenítése
-    echo "Hello, $fnev!";
+    echo "Hello, $fnev!<br>";
+    echo '<a href="profile.php">Profil szerkesztés</a><br>';
+    echo '<a href="admin.php">Admin panel</a>';
 } else {
     // Ha nincs bejelentkezve, akkor jelenítse meg a "Bejelentkezés" és "Regisztráció" linkeket
     echo '<a href="register.php">REGISZTRÁCIÓ</a><br>';
