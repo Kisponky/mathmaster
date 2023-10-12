32287782433d2d65f59ac8b90f31ccc0f2e9dc0b
LioMes1001 <tamasnadas04@gmail.com>
2023-10-12 21:39:50 +0200

    Reszponzívitás és kommentelés(index.php, styles.css)

--- Végleges/index.php
+++ Végleges/index.php
@@ -1,5 +1,4 @@
 <?php
-
 // Adatbázis kapcsolás
 $db = new mysqli('localhost', 'root', 'secret', 'jatekosmatek');
 
@@ -45,41 +44,52 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 $db->close();
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="hu">
 <!--Hooty a bagoly neve-->
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- Hamburger menü külső scriptek -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
+    <!-- Bootstrap külső hivatkozás -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Saját CSS -->
     <link rel="stylesheet" href="css/styles.css">
-
+    <!-- favicon -->
     <link rel="shortcut icon" href="img/owl-47526.svg">
 
     <title>MathMaster</title>
 </head>
 
 <body>
+    <!-- A fejléc tartalmazza a menü rendszert -->
     <header>
-        <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <!-- Navigációs sáv, Hamburger menü megjelenése sm méretben,
+        világos szürke háttér -->
+        <nav class="navbar navbar-expand-sm bg-light">
+            <!-- cég, projekt nevéhez, alsó margó 0, h1 méretű szöveg -->
             <a href="#" class="navbar-brand mb-0 h1">
+                <!-- MathMaster_Logo_nav: kis ikon bel oldalt -->
                 <img class="MathMaster_Logo_nav d-inline-block align-top" src="img/owl-47526.svg" alt="MathMaster Logó">
-                <!--nem középen jelenik meg-->
+                <!-- szöveg függölegesen középre igazítva -->
                 <span class="align-middle">MathMaster</span>
             </a>
+            <!-- navbar-toggler: összecsukási beépülő modul, hamburger gomb -->
             <button class="navbar-toggler second-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23" aria-expanded="false" aria-label="Toggle navigation">
+                <!-- animáció a gombra -->
                 <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
             </button>
+            <!-- elrejti a hamburger menüben a menü pontokat -->
             <div class="collapse navbar-collapse" id="navbarSupportedContent23">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                         <a class="nav-link" href="#">Kezdőlap</a>
                     </li>
-                    <?php 
+                    <?php
                     session_start();
                     if (isset($_SESSION['user_fnev'])) {
                         echo '<li class="nav-item"><a class="nav-link" href="php/profile.php">Profil</a></li>';
@@ -94,23 +104,30 @@ $db->close();
         </nav>
     </header>
 
+
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <img class="MathMaster_Logo img-fluid" src="img/owl-47526.svg" alt="MathMaster Logó">
+                <!-- Az oldal címe -->
                 <h1 class="cim">MathMaster</h1>
             </div>
         </div>
 
+
         <div class="row">
             <div class="col-lg-12 text-center">
+                <!-- következő rész címe -->
                 <h3 class="feladat_cim">Kezdj tanulni Hootyval kis baglyocska!</h3>
                 <img class="img-fluid" src="img/undraw_educator_re_ju471.svg" alt="Tanulás">
             </div>
         </div>
         <div class="row text-center">
+            <!-- md méret és az alatt eltűnik az elem, míg lg méret és az felett megjelenik az elem -->
             <div class="col-lg-3 d-md-none d-lg-block"></div>
             <div class="col-lg-3 col-md-6 col-sm-12 mt-3">
+                <!-- btn-block-sm: gomb teljes oldal szélességű telefonos nézetben,
+                osztaly_gomb: a gombok alapértelmezet stílusa -->
                 <a href="" class="btn btn-danger btn-block-sm osztaly_gomb">1-2 osztály</a>
             </div>
             <div class="col-lg-3 col-md-6 col-sm-12 mt-3">
@@ -127,14 +144,17 @@ $db->close();
             <div class="col-lg-3 d-md-none d-lg-block"></div>
         </div>
 
+
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2>Rólunk:</h2>
                 <p>Üdvözöljük a MathMaster online platformján! Válassz minket, mert szenvedélyünk az oktatás, és ezt a rendkívül hasznos tanulási oldalt életre hoztuk azért, hogy segítsünk neked a matematika világában.</p>
                 <p>Tudjuk, hogy a matematika néha kihívást jelent, de mi itt vagyunk, hogy segítsünk neked.</p>
+                <!-- Al részek gombja -->
                 <a href="" class="btn btn-secondary info_gomb">Tudj meg többet</a>
             </div>
         </div>
+
 
         <div class="row">
             <div class="col-lg-12 text-center">
@@ -146,11 +166,12 @@ $db->close();
         </div>
     </div>
 
+
     <footer>
         &copy 2023 MathMaster
     </footer>
 
-
+    <!-- Hamburger menü saját scriptje -->
     <script src="js/index.js"></script>
 </body>
 
