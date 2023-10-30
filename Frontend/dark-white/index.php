9c11d69841b46a1869ef6f62cdffe6b0c172d8bd
Kisponky <ponkhazibanri@gmail.com>
2023-10-30 16:43:05 +0100

    Világos és sötét mód megfelelő ikon megjelenítése a téma választóban

--- Frontend/dark-white/index.php
+++ Frontend/dark-white/index.php
@@ -43,9 +43,23 @@ $mode = $_SESSION['mode'];
     <form method="post">
         <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
             <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" aria-label="Téma váltása (dark)">
-                <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
-                    <use href="#sun-fill-light"></use>
-                </svg>
+                
+                <?php
+                // Világos és sötét mód ikon megjelenítése megfelelő ikonnal
+                if ($_SESSION['mode'] === "light") {
+                ?>
+                    <svg class="bi my-1 theme-icon-active" width="1em" height="1em">
+                        <use href="#sun-fill-light"></use>
+                    </svg>
+                <?php
+                } else { ?>
+                    <svg class="bi me-2 opacity-50 theme-icon" width="1em" height="1em">
+                        <use href="#moon-stars-fill"></use>
+                    </svg>
+                <?php
+                } ?>
+
+
                 <span class="visually-hidden" id="bd-theme-text">
                     <font style="vertical-align: inherit;">
                         <font style="vertical-align: inherit;">Téma váltása</font>
