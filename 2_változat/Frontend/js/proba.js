e680306e8c73bf62f23079263adf768c7540a162
LioMes1001 <tamasnadas04@gmail.com>
2024-02-23 09:00:47 +0100

    Írásbeli szorzás 4. osztály

--- 2_változat/Frontend/js/proba.js
+++ 2_változat/Frontend/js/proba.js
@@ -1,4 +1,4 @@
-var feladvany = "144*6=864";
+var feladvany = "8*88=704";
 var szamok = feladvany.match(/\d+/g);
 var muvJelek = feladvany.match(/[+\-*/]/g);
 
@@ -12,6 +12,7 @@ console.log("Második szám karakterei: ", masodikTag[1]);
 var elsoOsztaly = feladvany.match(/[+-]/g);
 var harmadikOsztaly = feladvany.match(/[/]/g);
 var harmadikOsztaly1 = feladvany.match(/[*]/g);
+var negyedikOsztaly = feladvany.match(/[*]/g);
 
 var kisebbMint20 = true;
 var kisebbMint100 = true;
@@ -78,7 +79,7 @@ if ((localStorage.getItem('class') == 1 && elsoOsztaly && kisebbMint20 == true /
     });
 
 
-} else if ((localStorage.getItem('class') == 3 && kisebbMint1000 == true && elsoOsztaly /*&& lekérdezés adatbázisból*/) || (localStorage.getItem('class') == 4 && kisebbMint10000 == true /*&& lekérdezés adatbázisból*/)) {
+} else if ((localStorage.getItem('class') == 3 && kisebbMint1000 == true && elsoOsztaly /*&& lekérdezés adatbázisból*/) || (localStorage.getItem('class') == 4 && kisebbMint10000 == true && elsoOsztaly /*&& lekérdezés adatbázisból*/)) {
     var elsoTagInput = '';
     for (let i = 0; i < szamok[0].length; i++) {
         elsoTagInput += `<input type="text" class="form-control tag-style" value="${elsoTag[i]}" disabled>`;
@@ -184,7 +185,7 @@ if ((localStorage.getItem('class') == 1 && elsoOsztaly && kisebbMint20 == true /
 
 
 
-} else if ((localStorage.getItem('class') == 3 && kisebbMint1000 == true && harmadikOsztaly1 && szamok[1].length == 1 /*&& lekérdezés adatbázisból*/)) {
+} else if ((localStorage.getItem('class') == 3 && kisebbMint1000 == true && harmadikOsztaly1 && szamok[1].length == 1 /*&& lekérdezés adatbázisból*/) || (localStorage.getItem('class') == 4 && kisebbMint10000 == true && negyedikOsztaly && szamok[1].length == 1 /*&& lekérdezés adatbázisból*/)) {
     var elsoTagInput = '';
     for (let i = 0; i < szamok[0].length; i++) {
         elsoTagInput += `<input type="text" class="form-control tag-style" value="${elsoTag[i]}" disabled>`;
@@ -291,6 +292,197 @@ if ((localStorage.getItem('class') == 1 && elsoOsztaly && kisebbMint20 == true /
         input.style.width = '20px';
         input.style.display = 'inline-block';
         input.style.margin = '0 2px';
+        input.style.padding = '3px 5px';
+    });
+} else if ((localStorage.getItem('class') == 4 && kisebbMint10000 == true && negyedikOsztaly && szamok[1].length == 2 /*&& lekérdezés adatbázisból*/)) {
+    var elsoTagInput = '';
+    for (let i = 0; i < szamok[0].length; i++) {
+        elsoTagInput += `<input type="text" class="form-control tag-style" value="${elsoTag[i]}" disabled>`;
+    }
+
+    var muvJelInput = `<input type="text" class="form-control tag-style" value="${muvJelek[0]}" disabled>`;
+
+
+    var masodikTagInput = '';
+    for (let i = 0; i < szamok[1].length; i++) {
+        masodikTagInput += `<input type="text" class="form-control tag-style" value="${masodikTag[i]}" disabled>`;
+    }
+
+    // Létrehozunk egy üres stringet, amelybe a ciklus eredményét fűzzük majd
+    var reszeredmenyInput1 = '';
+    var reszeredmeny1 = szamok[0] * (Math.floor(szamok[1] / 10));
+
+    // Az input mezők számának meghatározása
+    var inputCount1 = reszeredmeny1.toString().length;
+
+    // Fordított sorrendben beállítjuk a tabindex értéket az input mezőkön
+    for (let i = 0; i < inputCount1; i++) {
+        let reversedTabIndex1 = inputCount1 - i;
+        reszeredmenyInput1 += `<input type="text" class="form-control mt-1 input-style" maxlength="1" oninput="this.value = this.value.replace(/[^0-9]/g, '')" if(this.value.length === 1) { document.querySelector('input[tabindex=\"${reversedTabIndex1}\"] + input').focus(); }" tabindex="${reversedTabIndex1}">`;
+    }
+
+    var plus1 = szamok[1].length + 1;
+
+    for (let i = 0; i < plus1; i++) {
+        reszeredmenyInput1 += `<input type="text" class="form-control tag-style" disabled>`;
+    }
+
+    
+    
+    
+    // Létrehozunk egy üres stringet, amelybe a ciklus eredményét fűzzük majd
+    var reszeredmenyInput2 = '';
+    var reszeredmeny2 = szamok[0] * (Math.floor(szamok[1] % 10));
+
+    // Az input mezők számának meghatározása
+    var inputCount2 = reszeredmeny2.toString().length;
+    var plus2 = szamok[1].length;
+
+    var maradek = (szamok[0].length + szamok[1].length + 1) - (inputCount2 + plus2);
+    
+    if (maradek != 0) {
+        for (let i = 0; i < maradek; i++) {
+            reszeredmenyInput2 += `<input type="text" class="form-control tag-style" disabled>`;
+        }
+    } else {
+        for (let i = 0; i < maradek+1; i++) {
+            reszeredmenyInput2 += `<input type="text" class="form-control tag-style" disabled>`;
+        }
+    }
+
+    // Fordított sorrendben beállítjuk a tabindex értéket az input mezőkön
+    for (let i = 0; i < inputCount2; i++) {
+        let reversedTabIndex2 = inputCount1 - i;
+        reszeredmenyInput2 += `<input type="text" class="form-control mt-1 input-style" maxlength="1" oninput="this.value = this.value.replace(/[^0-9]/g, '')" if(this.value.length === 1) { document.querySelector('input[tabindex=\"${reversedTabIndex2}\"] + input').focus(); }" tabindex="${reversedTabIndex2}">`;
+    }
+
+    
+
+    for (let i = 0; i < plus2; i++) {
+        reszeredmenyInput2 += `<input type="text" class="form-control tag-style" disabled>`;
+    }
+
+    
+    
+    
+    // Létrehozunk egy üres stringet, amelybe a ciklus eredményét fűzzük majd
+    var eredmenyInput = '';
+
+    // Az input mezők számának meghatározása
+    var inputCount = szamok[2].length;
+
+    // Fordított sorrendben beállítjuk a tabindex értéket az input mezőkön
+    for (let i = 0; i < inputCount; i++) {
+        let reversedTabIndex = inputCount - i;
+        eredmenyInput += `<input type="text" id="eredmeny" class="form-control mt-1 input-style" maxlength="1" oninput="this.value = this.value.replace(/[^0-9]/g, '')" if(this.value.length === 1) { document.querySelector('input[tabindex=\"${reversedTabIndex}\"] + input').focus(); }" tabindex="${reversedTabIndex}">`;
+    }
+
+    var plus = szamok[1].length;
+
+    for (let i = 0; i < plus; i++) {
+        eredmenyInput += `<input type="text" class="form-control tag-style" disabled>`;
+    }
+
+    // Az input mezőkre vonatkozó fókuszkezelő beállítása
+    document.addEventListener("DOMContentLoaded", function () {
+        const inputFields = document.querySelectorAll('.input-style');
+        const button = document.querySelector('button');
+
+        inputFields.forEach((input, index) => {
+            input.addEventListener('input', function (event) {
+                if (event.target.value.length === 1) {
+                    const nextInput = inputFields[index - 1];
+                    if (nextInput) {
+                        nextInput.focus();
+                    } else {
+                        let result = '';
+                        inputFields.forEach(input => {
+                            result += input.value;
+                        });
+                        console.log("Eredmény: ", result);
+                    }
+                }
+            });
+        });
+
+        // button.addEventListener('click', function () {
+        //     let result = '';
+        //     inputFields.forEach(input => {
+        //         result += input.value;
+        //         input.disabled = true;
+        //     });
+
+        //     if (result === szamok[2]) {
+        //         console.log("Jó válasz!");
+        //         button.style.backgroundColor = 'rgb(0, 110, 0)';
+        //         button.textContent = 'Jó válasz!';
+        //     } else {
+        //         console.log("Rossz válasz!");
+        //         button.style.backgroundColor = 'red';
+        //         button.textContent = 'Rossz válasz!';
+        //     }
+        // });
+
+
+        button.addEventListener('click', function () {
+            // Az összes input elem kiválasztása a "Eredmény" id-vel rendelkező div alatt
+            var inputMezok = document.querySelectorAll('#eredmeny input[type="text"]');
+
+            // Az összes input mező értékeinek kinyerése és összevonása egy stringbe
+            var result = "";
+            inputMezok.forEach(function (input) {
+                result += input.value;
+                input.disabled = true;
+            });
+
+            if (result === szamok[2]) {
+                console.log("Jó válasz!");
+                button.style.backgroundColor = 'rgb(0, 110, 0)';
+                button.textContent = 'Jó válasz!';
+            } else {
+                console.log("Rossz válasz!");
+                button.style.backgroundColor = 'red';
+                button.textContent = 'Rossz válasz!';
+            }
+        });
+    });
+
+
+
+    // Az eredményHTML tartalmát illesszük be a tartalom elembe
+    document.getElementById("tartalom").innerHTML = `
+    <div class="col-2 col-md-4 col-lg-5"></div>
+    <div class="col-6 col-md-3 col-lg-2  text-end">
+        <div id="elso">${elsoTagInput + muvJelInput + masodikTagInput}</div>
+        <hr class="my-0">
+        <div>${reszeredmenyInput1}</div>
+        <div>+${reszeredmenyInput2}</div>
+        <hr class="my-0">
+        <div id="eredmeny">${eredmenyInput}</div>
+    </div>
+    <div class="col-4 col-md-5 col-lg-5 align-self-center">
+        <button class="btn btn-danger" type="button">Eredmény</button>
+    </div>
+    `;
+
+    const button = document.querySelector('button');
+
+    // Add CSS styles to the input elements
+    const tag = document.querySelectorAll('.tag-style');
+    tag.forEach(input => {
+        input.style.width = '20px';
+        input.style.display = 'inline-block';
+        input.style.margin = '0px 2px';
+        input.style.padding = '1px 5px';
+        input.style.border = 'none';
+        input.style.background = 'none';
+    });
+
+    const inputs = document.querySelectorAll('.input-style');
+    inputs.forEach(input => {
+        input.style.width = '20px';
+        input.style.display = 'inline-block';
+        input.style.margin = '0 2px 1px 2px';
         input.style.padding = '3px 5px';
     });
 }
