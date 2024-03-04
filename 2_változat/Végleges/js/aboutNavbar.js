cf7af018aadbfd27afc5914c3d486ed0ce025ff9
LioMes1001 <tamasnadas04@gmail.com>
2024-03-04 20:43:24 +0100

    about.html-hez tartozó részek tiszta kódjának elkészítése

---
+++ 2_változat/Végleges/js/aboutNavbar.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded", function () {
+    navBar();
+});
+
+function navBar() {
+    var navbarMenu = document.getElementById('navbarMenu');
+    var uzeneteim = document.getElementById('uzeneteim');
+    var admin = document.getElementById('admin');
+
+    navbarMenu.innerHTML = '';
+    uzeneteim.innerHTML = '';
+
+    // Felhasználó bejelentkezésének ellenőrzése
+    if (localStorage.getItem("token")) {
+        navbarMenu.innerHTML += getMenuHTML();
+    } else {
+        navbarMenu.innerHTML += getMenuHTML(false);
+    }
+
+
+
+    // Profilom menü elrejtése, ha a felhasználó nincs bejelentkezve
+    if (!localStorage.getItem("token")) {
+        document.getElementById("profilom").style.display = "none";
+    } else {
+        document.getElementById("profilom").style.display = "block";
+    }
+
+
+
+    // Üzenetek menüpont megjelenítése
+    if (!localStorage.getItem("admin")) {
+        uzeneteim.innerHTML += '<li><a class="dropdown-item" href="./messages.html">Üzeneteim</a></li>';
+    }
+
+
+
+    // Admin menüpont megjelenítése
+    if (localStorage.getItem("admin")) {
+        admin.innerHTML += '<li><a class="dropdown-item" href="./admin.html">Admin</a></li>';
+    }
+}
+
+// Függvény a menüpontok HTML kódjának generálásához
+function getMenuHTML(isLoggedIn = true) {
+    var menuHTML='';
+    menuHTML += '<li class="nav-item"><a class="nav-link" aria-current="page" href="../index.html">Kezdőlap</a></li>';
+    if (isLoggedIn) {
+        menuHTML += '<li class="nav-item"><a class="nav-link active" href="#">Rólunk</a></li>';
+        menuHTML += '<li class="nav-item"><a class="nav-link" href="./contact.html">Kapcsolat</a></li>';
+        menuHTML += '<li class="nav-item"><a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#uzenet">Üzenet küldés</a></li>';
+    } else {
+        menuHTML += '<li class="nav-item"><a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#regisztracio">Regisztráció</a></li>';
+        menuHTML += '<li class="nav-item"><a class="nav-link" href="#" data-bs-toggle="modal" data-bs-target="#bejelentkezes">Bejelentkezés</a></li>';
+        menuHTML += '<li class="nav-item"><a class="nav-link active" href="#">Rólunk</a></li>';
+    }
+    return menuHTML;
+}
